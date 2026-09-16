2897828ff01642bc6f9803d9261824968696bddf
Ole H. <91332299+olehrstm@users.noreply.github.com>
2026-09-16 20:50:53 +0200

    chore: initial project setup

---
+++ build.gradle.kts
@@ -0,0 +1,69 @@
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+    `java-library`
+    `maven-publish`
+}
+
+group = "de.ole101.lodestone"
+version = providers.gradleProperty("lodestone_version").get()
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // This dependency is exported to consumers, that is to say found on their compile classpath.
+    api(libs.commons.math3)
+
+    // This dependency is used internally, and not exposed to consumers on their own compile classpath.
+    implementation(libs.guava)
+}
+
+tasks {
+    jar {
+        val projectName = project.name
+        inputs.property("projectName", projectName)
+
+        from("LICENSE") {
+            rename { "${it}_$projectName" }
+        }
+    }
+}
+
+java {
+    withSourcesJar()
+}
+
+kotlin {
+    jvmToolchain(25)
+    explicitApi()
+}
+
+publishing {
+    publications {
+        register<MavenPublication>("mavenJava") {
+            from(components["java"])
+
+            artifactId = "lodestone"
+        }
+    }
+
+    repositories {
+        val reposiliteUrl = System.getenv("REPOSILITE_URL")
+        if (!reposiliteUrl.isNullOrBlank()) {
+            maven {
+                name = "reposilite"
+                url = uri(reposiliteUrl)
+
+                credentials {
+                    username = System.getenv("REPOSILITE_USERNAME")
+                    password = System.getenv("REPOSILITE_PASSWORD")
+                }
+
+                authentication {
+                    create<BasicAuthentication>("basic")
+                }
+            }
+        }
+    }
+}
---
+++ src/main/kotlin/de/ole101/lodestone/Library.kt
@@ -0,0 +1,7 @@
+package de.ole101.lodestone
+
+public class Library {
+    public fun someLibraryMethod(): Boolean {
+        return true
+    }
+}
