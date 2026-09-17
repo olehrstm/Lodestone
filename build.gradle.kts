9628084a181f9c7c93b3c9c094b440cdc7e739ec
Ole H. <91332299+olehrstm@users.noreply.github.com>
2026-09-17 20:22:15 +0200

    feat: add command dsl (#4)

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,11 +12,7 @@ repositories {
 }
 
 dependencies {
-    // This dependency is exported to consumers, that is to say found on their compile classpath.
-    api(libs.commons.math3)
-
-    // This dependency is used internally, and not exposed to consumers on their own compile classpath.
-    implementation(libs.guava)
+    implementation(libs.minestom)
 }
 
 tasks {
