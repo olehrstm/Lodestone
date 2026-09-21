62a38506640820de68e4aa6376904456415f0422
Ole H. <91332299+olehrstm@users.noreply.github.com>
2026-09-21 15:18:37 +0200

    feat: add mini message extensions

---
+++ src/main/kotlin/de/ole101/lodestone/text/MiniMessageExtensions.kt
@@ -0,0 +1,25 @@
+package de.ole101.lodestone.text
+
+import net.kyori.adventure.audience.Audience
+import net.kyori.adventure.text.Component
+
+public fun String.mini(): Component {
+    return MiniMessageProvider.parse(this)
+}
+
+public fun String.component(): Component {
+    return Component.text(this)
+}
+
+public fun Audience.sendMini(message: String) {
+    this.sendMessage(message.mini())
+}
+
+public fun String.sendMini(vararg audiences: Audience) {
+    val component = this.mini()
+    audiences.forEach { it.sendMessage(component) }
+}
+
+public fun Component.send(vararg audiences: Audience) {
+    audiences.forEach { it.sendMessage(this) }
+}
