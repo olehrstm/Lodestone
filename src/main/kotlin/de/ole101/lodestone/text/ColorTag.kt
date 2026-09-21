be8cca86c4ab124b177a38cfcc8d1715138b7e9e
Ole H. <91332299+olehrstm@users.noreply.github.com>
2026-09-21 15:17:53 +0200

    feat: add custom html colors and color tag

---
+++ src/main/kotlin/de/ole101/lodestone/text/ColorTag.kt
@@ -0,0 +1,30 @@
+package de.ole101.lodestone.text
+
+import net.kyori.adventure.text.format.TextColor
+import net.kyori.adventure.text.minimessage.Context
+import net.kyori.adventure.text.minimessage.tag.Tag
+import net.kyori.adventure.text.minimessage.tag.resolver.ArgumentQueue
+import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver
+
+public class ColorTag : TagResolver {
+
+    override fun resolve(
+        name: String,
+        arguments: ArgumentQueue,
+        ctx: Context
+    ): Tag? {
+        val color = this.colorFor(name) ?: return null
+
+        return Tag.styling(color)
+    }
+
+    override fun has(name: String): Boolean {
+        return this.colorFor(name) != null
+    }
+
+    private fun colorFor(name: String): TextColor? {
+        if (!name.startsWith("!")) return null
+
+        return Colors.named(name.substring(1))
+    }
+}
