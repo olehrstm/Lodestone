15f428db69e8078b6d160fa7b3f0f08b6a9531fc
Ole H. <91332299+olehrstm@users.noreply.github.com>
2026-09-24 22:11:30 +0200

    test: move test server to test source set

--- src/main/kotlin/de/ole101/lodestone/Library.kt
+++
@@ -1,7 +0,0 @@
-package de.ole101.lodestone
-
-public class Library {
-    public fun someLibraryMethod(): Boolean {
-        return true
-    }
-}
---
+++ src/test/kotlin/de/ole101/lodestone/main.kt
@@ -0,0 +1,39 @@
+package de.ole101.lodestone
+
+import de.ole101.lodestone.text.mini
+import de.ole101.lodestone.text.send
+import de.ole101.lodestone.text.sendMini
+import net.minestom.server.Auth
+import net.minestom.server.MinecraftServer
+import net.minestom.server.coordinate.Pos
+import net.minestom.server.event.player.AsyncPlayerConfigurationEvent
+import net.minestom.server.event.player.PlayerSpawnEvent
+import net.minestom.server.instance.LightingChunk
+import net.minestom.server.instance.block.Block
+
+fun main() {
+    val server = MinecraftServer.init(Auth.Online())
+
+    val instanceContainer = MinecraftServer.getInstanceManager().createInstanceContainer()
+    instanceContainer.setChunkSupplier(::LightingChunk)
+    instanceContainer.setGenerator { unit -> unit.modifier().fillHeight(1, 40, Block.GRASS_BLOCK) }
+
+//    MinecraftServer.getCommandManager().register(TestCommand())
+
+    MinecraftServer.getGlobalEventHandler().addListener(AsyncPlayerConfigurationEvent::class.java, { event ->
+        val player = event.player
+
+        event.spawningInstance = instanceContainer
+        player.respawnPoint = Pos(0.0, 42.0, 0.0)
+    })
+
+    MinecraftServer.getGlobalEventHandler().addListener(PlayerSpawnEvent::class.java, { event ->
+        val player = event.player
+
+        val message = "test123 <!red>".mini()
+        message.send(player)
+        "<small>Hello 123!</small>".sendMini(player)
+    })
+
+    server.start("0.0.0.0", 25565)
+}
